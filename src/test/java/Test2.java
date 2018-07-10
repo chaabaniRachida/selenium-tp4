4e78b27401f4c671dfe536823aa53cc9af736e6c
chaabaniRachida <chaabani.mjs@gmail.com>
2018-07-10 15:16:34 +0200

    close chrome

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test2 {
+    WebDriver driver;
+
+    @Test
+    public void t1(){
+        driver =new ChromeDriver();
+        driver.get("https://www.google.com");
+    }
 }
